1ad989c0366033e558484b4e6f22978a7b68c9f7
Cory Stever <cstever@hotmail.com>
2023-02-20 17:06:00 -0600

    Added Route for Get Spots Owned by Current User

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -48,4 +48,51 @@ router.get('/', async (req, res) =>{
     res.json({Spots: payload});
 });
 
+router.get('/current', requireAuth, handleValidationErrors, async (req, res) => {
+    const {id} = req.user;
+
+    const Spots = await Spot.findAll({
+        where: {
+            ownerId: id
+        }
+    });
+
+    let payload = [];
+    for (let spot of Spots) {
+        let spotJson = spot.toJSON();
+        const spotRating = await Review.findOne({
+            where: {
+                spotId: spot.id
+            },
+            attributes: [
+                [sequelize.fn('AVG', sequelize.col('stars')), 'avgRating']
+            ]
+        });
+
+        let rating = spotRating.dataValues.avgRating
+        if (!rating) {
+            spotJson.avgRating = 'No reviews yet'
+        } else {
+            spotJson.avgRating = rating
+        };
+
+        let preImage = await SpotImage.findOne({
+            where: {
+                spotId: spot.id,
+                preview: true
+            },
+            attributes: ['url']
+        });
+        if (!preImage) {
+            spotJson.previewImage = 'No preview available'
+        } else {
+            spotJson.previewImage = preImage.url
+        };
+
+        payload.push(spotJson);
+    };
+
+    res.json({Spots: payload});
+})
+
 module.exports = router;
