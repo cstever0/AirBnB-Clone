5ed900babe1828d5bc52b817f9381a9435a7f7ff
Cory Stever <cstever@hotmail.com>
2023-03-13 20:03:31 -0500

    Updating Seed Files

--- backend/db/seeders/20230220205043-reviews-data.js
+++ backend/db/seeders/20230220205043-reviews-data.js
@@ -11,38 +11,38 @@ module.exports = {
     options.tableName = 'Reviews';
     return queryInterface.bulkInsert(options, [
       {
-        spotId: 1,
-        userId: 1,
+        spotId: 1, // Cloud's Apartment
+        userId: 2, // Tifa
         review: 'Lovely place, great location',
         stars: 5
       },
       {
-        spotId: 2,
-        userId: 2,
+        spotId: 2, // Tifa's Apartment
+        userId: 1, // Cloud
         review: 'Lovely place, great location',
         stars: 5
       },
       {
-        spotId: 3,
-        userId: 3,
+        spotId: 3, // Seventh Heaven (Barret)
+        userId: 1, // Cloud
         review: 'Great place to throw a party, would recommend',
         stars: 5
       },
       {
-        spotId: 4,
-        userId: 2,
+        spotId: 4, // Tifa's House
+        userId: 3, // Barret
         review: 'Lovely place, great location',
         stars: 5
       },
       {
-        spotId: 5,
-        userId: 1,
+        spotId: 5, // Cloud's House
+        userId: 2, // Tifa
         review: 'Lovely place, great location',
         stars: 5
       },
       {
-        spotId: 1,
-        userId: 3,
+        spotId: 1, // Cloud's Apartment
+        userId: 2, // Tifa
         review: 'I was accosted by a man in black robes mumbling something about a reunion',
         stars: 1
       },
