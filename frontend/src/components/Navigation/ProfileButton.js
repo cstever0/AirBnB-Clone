99b929ba8c0e76bd94eab6bc60533121e40d1648
Cory Stever <cstever@hotmail.com>
2023-03-13 19:06:43 -0500

    Added Manage Spots to Profile Menu

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -5,6 +5,7 @@ import * as sessionActions from '../../store/session';
 import OpenModalMenuItem from './OpenModalMenuItem';
 import LoginFormModal from '../LoginFormModal';
 import SignupFormModal from '../SignupFormModal';
+import { NavLink } from "react-router-dom";
 
 function ProfileButton({ user }) {
     const dispatch = useDispatch();
@@ -55,9 +56,9 @@ function ProfileButton({ user }) {
                             <li>{user.username}</li>
                             <li>{user.firstName} {user.lastName}</li>
                             <li>{user.email}</li>
-                            <li>
-                                <button onClick={logout}>Log Out</button>
-                            </li>
+                            <NavLink to="/spots/current">Manage Spots</NavLink>
+                            <button onClick={logout}>Log Out</button>
+
                         </>
                     ) : (
                         <div className="modal-menu">
