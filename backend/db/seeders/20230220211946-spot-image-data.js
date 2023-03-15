9320afb04773a2a9a8c3f784eaa60e859035fe56
Cory Stever <cstever@hotmail.com>
2023-03-15 01:24:12 -0500

    Moved Image Seed Data to Cloudinary

--- backend/db/seeders/20230220211946-spot-image-data.js
+++ backend/db/seeders/20230220211946-spot-image-data.js
@@ -12,77 +12,77 @@ module.exports = {
     return queryInterface.bulkInsert(options, [
       {
         spotId: 1, //image url worked
-        url: 'https://ams0.files.sfmlab.com/content/content/image/preview2_dAz5vX1.png?AWSAccessKeyId=YALKTSWDUPLBGUZZ&Signature=ZWb6u2t0Na0aIx2F3rO84vp%2FqRE%3D&Expires=1678797510',
+        url: "https://res.cloudinary.com/dxcayvj3l/image/upload/v1678860963/cloud_tifa_apartments_nziyy1.png",
         preview: true,
       },
       {
         spotId: 1,
-        url: "/static/midgar_city.jpg",
+        url: "https://res.cloudinary.com/dxcayvj3l/image/upload/v1678860996/midgar_city_nm1szf.jpg",
         preview: false,
       },
       {
         spotId: 1,
-        url: '/static/clouds_apartment_door.jpg',
+        url: 'https://res.cloudinary.com/dxcayvj3l/image/upload/v1678860984/clouds_apartment_door_sny6zb.webp',
         preview: false,
       },
       {
         spotId: 2, //image url worked
-        url: 'https://ams0.files.sfmlab.com/content/content/image/preview2_dAz5vX1.png?AWSAccessKeyId=YALKTSWDUPLBGUZZ&Signature=ZWb6u2t0Na0aIx2F3rO84vp%2FqRE%3D&Expires=1678797510',
+        url: 'https://res.cloudinary.com/dxcayvj3l/image/upload/v1678860963/cloud_tifa_apartments_nziyy1.png',
         preview: true,
       },
       {
         spotId: 2, // image url worked
-        url: "/static/midgar_city.jpg",
+        url: "https://res.cloudinary.com/dxcayvj3l/image/upload/v1678860996/midgar_city_nm1szf.jpg",
         preview: false,
       },
       {
         spotId: 2,
-        url: "/static/tifas_apartment.jpg",
+        url: "https://res.cloudinary.com/dxcayvj3l/image/upload/v1678861007/tifas_apartment_ypusox.png",
         preview: false,
       },
       {
         spotId: 3,
-        url: "/static/seventh_heaven.jpg",
+        url: "https://res.cloudinary.com/dxcayvj3l/image/upload/v1678861007/seventh_heaven_sfmoun.jpg",
         preview: true,
       },
       {
         spotId: 3,
-        url: "/static/seventh_heaven.jpg",
+        url: "https://res.cloudinary.com/dxcayvj3l/image/upload/v1678861007/seventh_heaven_sfmoun.jpg",
         preview: false,
       },
       {
         spotId: 3,
-        url: "/static/midgar_city.jpg",
+        url: "https://res.cloudinary.com/dxcayvj3l/image/upload/v1678860996/midgar_city_nm1szf.jpg",
         preview: false,
       },
       {
         spotId: 4,
-        url: "/static/nibelheim_town.webp",
+        url: "https://res.cloudinary.com/dxcayvj3l/image/upload/v1678861007/nibelheim_town_gc2epm.webp",
         preview: true,
       },
       {
         spotId: 4,
-        url: "/static/tifas_house_entrance.webp",
+        url: "https://res.cloudinary.com/dxcayvj3l/image/upload/v1678861007/tifas_house_entrance_phxtc1.webp",
         preview: false,
       },
       {
         spotId: 4,
-        url: "/static/tifas_house_upstairs.webp",
+        url: "https://res.cloudinary.com/dxcayvj3l/image/upload/v1678861007/tifas_house_upstairs_gkh7sp.webp",
         preview: false,
       },
       {
         spotId: 5,
-        url: "/static/nibelheim_town.webp",
+        url: "https://res.cloudinary.com/dxcayvj3l/image/upload/v1678861007/nibelheim_town_gc2epm.webp",
         preview: true,
       },
       {
         spotId: 5,
-        url: "/static/clouds_house.webp",
+        url: "https://res.cloudinary.com/dxcayvj3l/image/upload/v1678860989/clouds_house_teyrmi.webp",
         preview: false,
       },
       {
         spotId: 5,
-        url: "/static/clouds_house.webp",
+        url: "https://res.cloudinary.com/dxcayvj3l/image/upload/v1678860989/clouds_house_teyrmi.webp",
         preview: false,
       },
     ], {});
