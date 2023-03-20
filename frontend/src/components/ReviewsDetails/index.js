2b2b054856568358bbb1c5183b0f37329883b15e
Cory Stever <cstever@hotmail.com>
2023-03-20 03:01:57 -0500

    Changed Date Formatting for reviews

--- frontend/src/components/ReviewsDetails/index.js
+++ frontend/src/components/ReviewsDetails/index.js
@@ -3,11 +3,18 @@ import OpenModalButton from "../OpenModalButton";
 import DeleteReviewModal from "../DeleteReviewModal/DeleteReviewModal";
 
 const ReviewDetails = ({ review, user }) => {
+    let date = new Date(review.createdAt);
+    let monthNames = ["January","February","March","April","May","June","July",
+    "August","September","October","November","December"];
+    let monthIdx = date.getMonth();
+    let month = monthNames[monthIdx];
+    let year = date.getFullYear();
+
     return (
         <div className="review-list">
             <div className="review-details">
                 <h2>{review.User.firstName}</h2>
-                <h3>{review.updatedAt}</h3>
+                <h3>{month}, {year}</h3>
                 <p>{review.review}</p>
             </div>
             <div className="delete-review-button">
