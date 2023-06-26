1108af2e935475cf68d4e25b861824bd0d8e0b42
Cory Stever <cstever@hotmail.com>
2023-06-26 16:15:24 -0500

    Spot Image Seed Data Added

--- backend/db/seeders/20230220205042-seed-spot-data.js
+++ backend/db/seeders/20230220205042-seed-spot-data.js
@@ -1,5 +1,12 @@
 'use strict';
 
+const fs = require('fs')
+
+const data = fs.readFileSync('db/mockData/spots.json',
+    { encoding: 'utf8', flag: 'r' });
+
+const dataObj = JSON.parse(data);
+
 let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
@@ -9,83 +16,7 @@ if (process.env.NODE_ENV === 'production') {
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     options.tableName = 'Spots';
-    return queryInterface.bulkInsert(options, [
-      {
-        ownerId: 1,
-        address: '123 Slums Lane',
-        city: 'Sector 7',
-        state: 'Midgar',
-        country: 'Eastern Continent',
-        lat: -33.868,
-        lng: 151.214,
-        name: "Cloud's Apartment",
-        description: 'Small apartment, no windows, located near Seventh Heaven',
-        price: 200.00
-      },
-      {
-        ownerId: 2,
-        address: '125 Slums Lane',
-        city: 'Sector 7',
-        state: 'Midgar',
-        country: 'Eastern Continent',
-        lat: -33.868,
-        lng: 151.214,
-        name: "Tifa's Apartment",
-        description: 'Small apartment, no windows, located near Seventh Heaven',
-        price: 200.00
-      },
-      {
-        ownerId: 3,
-        address: '100 Slums Lane',
-        city: 'Sector 7',
-        state: 'Midgar',
-        country: 'Eastern Continent',
-        lat: -33.868,
-        lng: 151.214,
-        name: 'Seventh Heaven',
-        description: 'Great place, full bar, arcade, darts, and workout room',
-        price: 500.00
-      },
-      {
-        ownerId: 2,
-        address: '123 Nibelheim Way',
-        city: 'Nibelheim',
-        state: 'North Western Area',
-        country: 'Western Continent',
-        lat: -33.868,
-        lng: 151.214,
-        name: "Tifa's House",
-        description: 'great house, full kitchen, multiple bedrooms',
-        price: 1.00
-      },
-      {
-        ownerId: 1,
-        address: '125 Nibelheim Way',
-        city: 'Nibelheim',
-        state: 'North Western Area',
-        country: 'Western Continent',
-        lat: -33.868,
-        lng: 151.214,
-        name: "Cloud's House",
-        description: 'great house, full kitchen, very large living area for gatherings',
-        price: 1.00
-      },
-      { ownerId: 4, address: "9 Monument Hill", city: "Paledang", country: "Indonesia", lat: -6.6000226, lng: 106.79719, name: "Blackbird", description: "Nam nulla.", price: 284134.46 },
-      { ownerId: 15, address: "77 6th Court", city: "Kiruru", country: "Indonesia", lat: -3.9389, lng: 134.9696, name: "Dunning", description: "Donec semper sapien a libero. Nam dui.", price: 494005.98 },
-      { ownerId: 11, address: "4058 Novick Road", city: "Nanxi", country: "China", lat: 28.845626, lng: 104.969882, name: "Rieder", description: "Proin at turpis a pede posuere nonummy.", price: 848048.09 },
-      { ownerId: 6, address: "424 Springview Road", city: "Anning", country: "China", lat: 24.919493, lng: 102.478494, name: "Havey", description: "Etiam faucibus cursus urna. Ut tellus.", price: 908376.60 },
-      { ownerId: 1, address: "82500 Northfield Center", city: "Vacaria", country: "Brazil", lat: -28.3276327, lng: -50.9289246, name: "Arapahoe", description: "Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.", price: 162029.30 },
-      { ownerId: 3, address: "681 Upham Park", city: "Wang Noi", country: "Thailand", lat: 14.2524816, lng: 100.7323332, name: "Drewry", description: "Etiam pretium iaculis justo. In hac habitasse platea dictumst.", "price": 760978.43 },
-      { ownerId: 1, address: "0584 Division Point", city: "Pindi Gheb", country: "Pakistan", lat: 33.237626, lng: 72.270844, name: "Erie", description: "Suspendisse potenti.", price: 447524.70 },
-      { ownerId: 2, address: "6 Reindahl Lane", city: "Pai do Vento", state: "Lisboa", country: "Portugal", lat: 38.7146303, lng: -9.414533, name: "Summer Ridge", description: "Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros. Vestibulum ac est lacinia nisi venenatis tristique.", price: 746536.74 },
-      { ownerId: 6, address: "85 Acker Road", city: "Besançon", state: "Franche-Comté", country: "France", lat: 47.2154159, lng: 5.9471867, name: "Golden Leaf", description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.", price: 113357.09 },
-      { ownerId: 2, address: "3143 Luster Parkway", city: "Phunphin", country: "Thailand", lat: 9.1118027, lng: 99.2065202, name: "Jenifer", description: "Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.", price: 723112.80 },
-      { ownerId: 7, address: "9196 Crownhardt Place", city: "Horodyshche", country: "Ukraine", lat: 49.2917766, lng: 31.4465327, name: "Mesta", description: "Maecenas pulvinar lobortis est. Phasellus sit amet erat.", price: 837073.71 },
-      { ownerId: 9, address: "24 Corry Trail", city: "Oropesa", country: "Peru", lat: -13.60277, lng: -71.7676089, name: "Talmadge", description: "Nullam sit amet turpis elementum ligula vehicula consequat.", price: 701089.87 },
-      { ownerId: 13, address: "1 Scott Parkway", city: "Monkayo", country: "Philippines", lat: 7.8394835, lng: 126.0502748, name: "Heffernan", description: "Etiam pretium iaculis justo. In hac habitasse platea dictumst.", price: 624036.24 },
-      { ownerId: 6, address: "4 Anderson Point", city: "Buôn Ma Thuột", country: "Vietnam", lat: 12.6661944, lng: 108.0382475, name: "Hansons", description: "Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend.", price: 47542.54 },
-      { ownerId: 14, address: "99461 Clyde Gallagher Lane", city: "Timiryazevo", country: "Kazakhstan", lat: 53.7512087, lng: 66.4866582, name: "Sycamore", description: "Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.", price: 625792.31 }
-    ], {});
+    return queryInterface.bulkInsert(options, dataObj, {});
   },
 
   down: async (queryInterface, Sequelize) => {
--- backend/db/seeders/20230220205043-reviews-data.js
+++ backend/db/seeders/20230220205043-reviews-data.js
@@ -1,5 +1,12 @@
 'use strict';
 
+const fs = require('fs')
+
+const data = fs.readFileSync('db/mockData/reviews.json',
+    { encoding: 'utf8', flag: 'r' });
+
+const dataObj = JSON.parse(data);
+
 let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
@@ -9,58 +16,7 @@ if (process.env.NODE_ENV === 'production') {
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     options.tableName = 'Reviews';
-    return queryInterface.bulkInsert(options, [
-      {
-        spotId: 1, // Cloud's Apartment
-        userId: 3, // Barret
-        review: 'Lovely place, great location',
-        stars: 5
-      },
-      {
-        spotId: 2, // Tifa's Apartment
-        userId: 1, // Cloud
-        review: 'Lovely place, great location',
-        stars: 5
-      },
-      {
-        spotId: 3, // Seventh Heaven (Barret)
-        userId: 1, // Cloud
-        review: 'Great place to throw a party, would recommend',
-        stars: 5
-      },
-      {
-        spotId: 4, // Tifa's House
-        userId: 3, // Barret
-        review: 'Lovely place, great location',
-        stars: 5
-      },
-      {
-        spotId: 5, // Cloud's House
-        userId: 2, // Tifa
-        review: 'Lovely place, great location',
-        stars: 5
-      },
-      {
-        spotId: 1, // Cloud's Apartment
-        userId: 2, // Tifa
-        review: 'I was accosted by a man in black robes mumbling something about a reunion',
-        stars: 1
-      },
-      { spotId: 8, userId: 3, review: "Vivamus vestibulum sagittis sapien.", stars: 2 },
-      { spotId: 14, userId: 2, review: "Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.", stars: 5 },
-      { spotId: 9, userId: 5, review: "In sagittis dui vel nisl. Duis ac nibh.", stars: 4 },
-      { spotId: 5, userId: 14, review: "Proin at turpis a pede posuere nonummy.", stars: 5 },
-      { spotId: 9, userId: 2,review: "Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc.", stars: 1 },
-      { spotId: 19, userId: 11, review: "In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.", stars: 5 },
-      { spotId: 19, userId: 14, review: "Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.", stars: 2 },
-      { spotId: 2, userId: 7, review: "Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.", stars: 5 },
-      { spotId: 2, userId: 4, review: "Phasellus in felis.", stars: 2 },
-      { spotId: 15, userId: 2, review: "Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.", stars: 5 },
-      { spotId: 13, userId: 11, review: "Mauris lacinia sapien quis libero. Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum.", stars: 1 },
-      { spotId: 6, userId: 12, review: "Sed ante. Vivamus tortor. Duis mattis egestas metus.", stars: 1 },
-      { spotId: 18, userId: 4, review: "Mauris sit amet eros. Suspendisse accumsan tortor quis turpis. Sed ante.", stars: 2 },
-      { spotId: 18, userId: 12, review: "Pellentesque viverra pede ac diam.", stars: 1 }
-    ], {});
+    return queryInterface.bulkInsert(options, dataObj, {});
   },
 
   down: async (queryInterface, Sequelize) => {
