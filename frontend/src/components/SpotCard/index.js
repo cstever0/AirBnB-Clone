e0986f90c6c875762ed9a36c9d4f61054276aac2
Cory Stever <cstever@hotmail.com>
2023-07-07 13:29:02 -0500

    SpotShow CSS Added

--- frontend/src/components/SpotCard/index.js
+++ frontend/src/components/SpotCard/index.js
@@ -18,7 +18,7 @@ export default function SpotCard({spot}) {
                         {spot.city}, {spot.state}
                     </div>
                     <div className="all-spot-rating">
-                        {Number.isInteger(spot.avgRating) ? <i className="fa fa-star">{spot.avgRating.toFixed(1)}</i> : 'No reviews yet'}
+                        {Number.isInteger(spot.avgRating) ? <><i className="fa fa-star"></i><span>{spot.avgRating.toFixed(1)}</span></> : 'No reviews yet'}
                     </div>
                 </div>
                 <div className="all-spot-price">${spot.price}/night</div>
